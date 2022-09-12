5902271cf2820dab3476ec06f75759280db8d519
Kevin Yeh <kevin.yay@gmail.com>
2022-09-11 21:11:12 -0700

    Fix unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
+import React from "react";
 import App from "./App";
+import { it, describe } from "vitest";
+
 import { render } from "@testing-library/react";
 
 describe("App", () => {
   it("renders the App title", () => {
     const { getByText } = render(<App />);
-    getByText("Hello world!");
+    getByText("Top Github Repositories by User");
   });
 });
